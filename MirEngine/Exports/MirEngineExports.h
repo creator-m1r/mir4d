0829f19e2aba2d45023b5ee61ecd1f2610cb5312
creator-m1r <creator.in.the.heart@gmail.com>
2026-08-14 03:03:27 -0700

    Connect MirEngine scene geometry command to OpenGL viewport API

--- MirEngine/Exports/MirEngineExports.h
+++ MirEngine/Exports/MirEngineExports.h
@@ -12,78 +12,31 @@ typedef struct {
     uint32_t height;
 } MirEngineSize2D;
 
-void* MirEngineCreateMacOpenGLContext(
-    void* view,
-    MirEngineSize2D size
-);
+void* MirEngineCreateMacOpenGLContext(void* view, MirEngineSize2D size);
+void MirEngineDestroyOpenGLContext(void* context);
+void* MirEngineCreateOpenGLRenderer(void* context);
+bool MirEngineInitializeRenderer(void* renderer);
+void MirEngineDestroyRenderer(void* renderer);
+void* MirEngineCreateViewport(void* renderer, uint32_t width, uint32_t height);
+void MirEngineDestroyViewport(void* viewport);
+void MirEngineRender(void* viewport);
+void MirEngineResize(void* viewport, uint32_t width, uint32_t height);
 
-void MirEngineDestroyOpenGLContext(
-    void* context
-);
-
-void* MirEngineCreateOpenGLRenderer(
-    void* context
-);
-
-bool MirEngineInitializeRenderer(
-    void* renderer
-);
-
-void MirEngineDestroyRenderer(
-    void* renderer
-);
-
-void* MirEngineCreateViewport(
-    void* renderer,
-    uint32_t width,
-    uint32_t height
-);
-
-void MirEngineDestroyViewport(
-    void* viewport
-);
-
-void MirEngineRender(
-    void* viewport
-);
-
-void MirEngineResize(
+// Document/scene geometry commands. Geometry is created in MirEngine's
+// OpenGL viewport Scene and is therefore rendered by the same renderer.
+bool MirEngineCreateBox(
     void* viewport,
-    uint32_t width,
-    uint32_t height
+    double width,
+    double depth,
+    double height,
+    uint64_t* objectId
 );
 
-void MirEngineViewportMouseDown(
-    void* viewport,
-    int button,
-    float x,
-    float y
-);
-
-void MirEngineViewportMouseUp(
-    void* viewport,
-    int button,
-    float x,
-    float y
-);
-
-void MirEngineViewportMouseMove(
-    void* viewport,
-    float x,
-    float y
-);
-
-void MirEngineViewportScroll(
-    void* viewport,
-    float delta
-);
-
-void MirEngineViewportClick(
-    void* viewport,
-    float x,
-    float y,
-    bool addToSelection
-);
+void MirEngineViewportMouseDown(void* viewport, int button, float x, float y);
+void MirEngineViewportMouseUp(void* viewport, int button, float x, float y);
+void MirEngineViewportMouseMove(void* viewport, float x, float y);
+void MirEngineViewportScroll(void* viewport, float delta);
+void MirEngineViewportClick(void* viewport, float x, float y, bool addToSelection);
 
 #ifdef __cplusplus
 }
