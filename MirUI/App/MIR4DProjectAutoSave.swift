3334a2288f8fcee0e38de15d2ee94c0d8f5239e9
creator-m1r <creator.in.the.heart@gmail.com>
2026-08-14 02:52:05 -0700

    Mark MIR 4D project dirty when model changes

--- MirUI/App/MIR4DProjectAutoSave.swift
+++ MirUI/App/MIR4DProjectAutoSave.swift
@@ -4,24 +4,28 @@ import Combine
 @MainActor
 final class MIR4DProjectAutoSave {
     private var timer: Timer?
+    private var modelObserver: NSObjectProtocol?
     private weak var appState: CADAppState?
     private let interval: TimeInterval
 
     init(appState: CADAppState, interval: TimeInterval = 5.0) {
         self.appState = appState
         self.interval = interval
+        modelObserver = NotificationCenter.default.addObserver(forName: .mir4DModelChanged, object: nil, queue: .main) { [weak self] _ in
+            self?.appState?.documentDirty = true
+        }
         start()
     }
 
     deinit {
         timer?.invalidate()
+        if let modelObserver { NotificationCenter.default.removeObserver(modelObserver) }
     }
 
     func start() {
         timer?.invalidate()
         timer = Timer.scheduledTimer(withTimeInterval: interval, repeats: true) { [weak self] _ in
-            guard let self, let appState = self.appState else { return }
-            guard appState.documentDirty else { return }
+            guard let self, let appState = self.appState, appState.documentDirty else { return }
             appState.saveMIR4DProject()
         }
     }
