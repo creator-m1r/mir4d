3bda2c11be2386fd0eb8b9c174bd17c680623f05
creator-m1r <creator.in.the.heart@gmail.com>
2026-08-14 23:37:52 +0300

    Delete MirEngine directory

--- MirEngine/Core/Core.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-// Canonical public Core API.
-#include "Types/Scalar.hpp"
-#include "Types/Angle.hpp"
-#include "Identity/ObjectId.hpp"
-#include "Identity/ObjectRegistry.hpp"
--- MirEngine/Rendering/Rendering.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// Canonical renderer public boundary.
-#include "Renderer.h"
-#include "Core/RenderContext.h"
-#include "Core/RenderDevice.h"
-#include "Passes/RenderPass.h"
-#include "Passes/GeometryPass.h"
-#include "Passes/GridPass.h"
